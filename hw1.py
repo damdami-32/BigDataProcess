ffa895f112ffd0f427a26638b9850c2e98f4531c
damhee <okcall0302@gmail.com>
2023-10-18 20:35:11 +0900

    commit test2

--- hw1.py
+++ hw1.py
@@ -9,3 +9,5 @@ for  row  in  range(2, row_max+1):
     ws['G' + str(row)].value = ws['C' + str(row)].value * 0.3 + ws['D' + str(row)].value * 0.35 + ws['E' + str(row)].value * 0.34 + ws['F' + str(row)].value
 
 wb.save('student.xlsx')
+
+#lab4_3 복사만 한 파일.. 시험 끝나고 과제하자
