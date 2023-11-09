180f0483670d62837531dc5be1a7a13dea5966f6
damdami-32 <99523828+damdami-32@users.noreply.github.com>
2023-11-09 17:59:23 +0900

    파일 입력 출력 오류 수정

--- HW2/IMDBStudent20210859.py
+++ HW2/IMDBStudent20210859.py
@@ -1,26 +1,21 @@
-input_name = input()     #movies_exp.txt
-output_name = input()      #movieoutput.txt
-
-input_file = open(input_name, "rt")
-output_file = open(output_name, "wt")
+import sys
 
 movie_dict = dict()
 
-for line in input_file:
-    line = line.strip()
+with open(sys.argv[1], "r") as input_file:
+    for line in input_file:
+        line = line.strip()
 
-    colon = line.split("::")
-    genre = colon[2]
-    detail_genre = genre.split("|")
-    
-    for item in detail_genre:
-        if item in movie_dict:
-            movie_dict[item] += 1
-        else:
-            movie_dict[item] = 1
+        colon = line.split("::")
+        genre = colon[2]
+        detail_genre = genre.split("|")
+        
+        for item in detail_genre:
+            if item in movie_dict:
+                movie_dict[item] += 1
+            else:
+                movie_dict[item] = 1
 
-for item in movie_dict:
-    output_file.write(f"{item} {str(movie_dict[item])}\n")
-
-input_file.close()
-output_file.close()
+with open(sys.argv[2], "w") as output_file:
+    for item in movie_dict:
+        output_file.write(f"{item} {movie_dict[item]}\n")
--- HW2/UBERStudent20210859.py
+++ HW2/UBERStudent20210859.py
@@ -1,28 +1,24 @@
 import datetime
 import sys
 
-input_file = open(sys.argv[1], "rt")
-output_file = open(sys.argv[2], "wt")
-
 day_lst = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 sort_lst = []
 
-for line in input_file:
-    line = line.strip()
+with open(sys.argv[1], "r") as input_file:
+    for line in input_file:
+        line = line.strip()
 
-    colon = line.split(",")
-    days = colon[1].split("/")
-    days = list(map(int, days))
-    day = datetime.date(int(days[2]), int(days[0]), int(days[1])).weekday()
+        colon = line.split(",")
+        days = colon[1].split("/")
+        days = list(map(int, days))
+        day = datetime.date(int(days[2]), int(days[0]), int(days[1])).weekday()
 
-    lst = [colon[0], day_lst[day], colon[2], colon[3]]
+        lst = [colon[0], day_lst[day], colon[2], colon[3]]
 
-    sort_lst.append(lst)
+        sort_lst.append(lst)
 
 #sort_lst.sort(key=lambda x: (x[0], x[1][2], x[1][0], x[1][1]))
 
-for item in sort_lst:
-    output_file.write(f"{item[0]},{item[1]} {item[2]},{item[3]}\n")
-
-input_file.close()
-output_file.close()
+with open(sys.argv[2], "w") as output_file:
+    for item in sort_lst:
+        output_file.write(f"{item[0]},{item[1]} {item[2]},{item[3]}\n")
