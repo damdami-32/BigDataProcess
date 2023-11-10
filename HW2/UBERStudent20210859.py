f10092c0e514a69f576293b963ff5c6e5486aa2c
damdami-32 <99523828+damdami-32@users.noreply.github.com>
2023-11-10 11:08:59 +0900

    번호, 요일별 합산 수정

--- HW2/UBERStudent20210859.py
+++ HW2/UBERStudent20210859.py
@@ -17,8 +17,33 @@ with open(sys.argv[1], "r") as input_file:
 
         sort_lst.append(lst)
 
-#sort_lst.sort(key=lambda x: (x[0], x[1][2], x[1][0], x[1][1]))
+sort_lst.sort(key=lambda x: (x[0], x[1][2], x[1][0], x[1][1]))
+
+result_lst = []
+pivot_lst = []
+
+for item in sort_lst:
+    flag = 1
+    pivot = [item[0], item[1]]
+    
+    for p in pivot_lst:
+        if pivot == p:
+            flag = 0
+
+    if flag:
+        car = 0
+        num = 0
+        for rec in sort_lst:
+            rec_lst = [rec[0], rec[1]]
+            if pivot == rec_lst:
+                car += int(rec[2])
+                num += int(rec[3])
+
+        lst = [pivot[0], pivot[1], car, num]
+        result_lst.append(lst)
+
+        pivot_lst.append(pivot)
 
 with open(sys.argv[2], "w") as output_file:
-    for item in sort_lst:
+    for item in result_lst:
         output_file.write(f"{item[0]},{item[1]} {item[2]},{item[3]}\n")
